b7ae6830bdeecb9ae64c375b4d0f73b08a6f92eb
Akhilthalappalli <65078187+Akhilthalappalli@users.noreply.github.com>
2021-12-09 17:34:12 +0530

    Update settings.py

--- Student/Student/settings.py
+++ Student/Student/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+
+
+# latest change
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
